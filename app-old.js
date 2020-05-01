d02ba0444eb0cf6bd7a3f6057999289ba894be1c
Henry <hveramendi@avatar-global.com>
2020-05-01 18:01:11 -0500

    primer commit

---
+++ app-old.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+            // res.write('Hola Mundo');
+        let salida = {
+            nombre: 'Ferando',
+            edad: 40,
+            url: req.url
+        }
+        res.write(JSON.stringify(salida))
+        res.end();
+    })
+    .listen(8080);
+console.log('Escuchando el puerto 8080');
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express(); //producto de la funcion de express
+const hbs = require('hbs');
+
+//MIDDELWARE -- es una instruccion o un callback que se va ejecutar siempre, no importa que URL la persona pida
+app.use(express.static(__dirname + '/public'))
+
+//EXPRESS HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+//usando helper
+require('./hbs/helpers');
+
+//variable puerto para heroku
+const port = process.env.PORT || 8080;
+
+//app.get('/'-- configurando una solicitud get cuando el pat sea /
+app.get('/', (req, res) => {
+
+    res.render('home', {
+        nombre: 'GEEEEEEEEE'
+    });
+});
+//otro path
+app.get('/about', (req, res) => {
+
+    res.render('about');
+});
+app.listen(port, () => {
+        console.log(`Escuchando el puerto ${ port} `);
+    }) //tambien recibe un callback
+
+//ejemplo con path
+// app.get('/data', (req, res) => {
+//     res.send('Hola Data');
+// });
